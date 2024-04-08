e183113967da71294628f9eae2c0c22021d98be7
yazmimmm <142514444+yazmimmm@users.noreply.github.com>
2024-04-08 20:04:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const botoes=document.querySelectorALL(".botao");
+
+
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick=function(){
+      
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classlist.remove("ativo");
+        }
+    
+        botoes[i].classlist.add("ativo");
+    
+    }
+
+
+
+
+
+
+
+
+
+}
